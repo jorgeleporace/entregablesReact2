2343f86135fd5816e6d5091f18817ec146fa5183
Jorge Enrique Leporace <67753773+jorgeleporace@users.noreply.github.com>
2023-02-08 13:57:53 -0300

    Empiezando con los diseños de Chackra

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,8 +1,17 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
 import React from 'react'
 
 const CartWidget = () => {
   return (
-    <div>Icono</div>
+    <div>
+    <Button colorScheme='teal' size='md'>
+    Shopping Cart 
+  
+    <span className="material-symbols-outlined">
+shopping_cart
+</span>
+</Button>
+</div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CartWidget from './CartWidget'
+import Image from '../assets/Logo_gls_512.png'
 
 const NavBar = () => {
   return (
     <div>
+      <img src={Image}alt=''/>
       <h1>Mi sitio de Ecommerce</h1>
       <ul>
         <li>Cat1</li>
